368fa2c511a6baba438b6d9ee92a00c4f2916c57
IleanaEscobar <izescobar5@gmail.com>
2022-06-20 13:33:53 -0700

    Before NutritionalLabel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
+import Header from './components/Header/Header'
+import Instructions from './components/Instructions/Instructions'
+import Chip from './components/Chip/Chip'
+import { useState } from 'react';
 
 // don't move this!
 export const appInfo = {
@@ -21,27 +25,64 @@ export const appInfo = {
 const { data, categories, restaurants } = createDataSet()
 
 export function App() {
+
+  const [foodStat, setFoodStat] = useState('')
+
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
       <div className="CategoriesColumn col">
         <div className="categories options">
           <h2 className="title">Categories</h2>
-          {/* YOUR CODE HERE */}
+          {
+            categories.map((item, indx) => (
+              <Chip
+                handleClick={() => {
+                  console.log('halo')
+                  setFoodStat(item)
+                }}
+                key={item}
+                label = {item}
+                isActive = {item == foodStat}
+              >{item}</Chip>
+            ))
+          }
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
-        {/* HEADER GOES HERE */}
+      {/* AppInfo={AppInfo} */}
+        <Header
+        title = {appInfo.title}
+        tagline = {appInfo.tagline}
+        description = {appInfo.description}/>
 
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">{
+            restaurants.map((item, indx) => (
+              <Chip
+              handleClick={() => {
+                console.log('halo')
+                setFoodStat(item)
+              }}
+              key={item}
+              label = {item}
+              isActive = {item == foodStat}>{item}</Chip>
+            ))
+          }</div>
         </div>
 
         {/* INSTRUCTIONS GO HERE */}
+        <Instructions
+        instructions = {appInfo.instructions.start}
+        // onlyCategory = {appInfo.instructions.onlyCategory}
+        // onlyRestaurant = {appInfo.instructions.onlyRestaurant}
+        // noSelectedItem = {appInfo.instructions.noSelectedItem}
+        // allSelected = {appInfo.instructions.allSelected}
+        />
 
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
